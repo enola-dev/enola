fb14b076d9bbefeceef21a51dd5d1b722f3c4cab
Michael Vorburger <mike@vorburger.ch>
2023-02-19 19:36:03 +0100

    k8s Java Proto example

--- k8s/src/main/java/dev/enola/k8s/Bek.java
+++ k8s/src/main/java/dev/enola/k8s/Bek.java
@@ -1,6 +1,10 @@
 package dev.enola.k8s;
 
+import io.kubernetes.client.ProtoClient;
+import io.kubernetes.client.ProtoClient.ObjectOrStatus;
 import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.Configuration;
+import io.kubernetes.client.proto.V1;
 import io.kubernetes.client.util.Config;
 
 public class Bek {
@@ -8,5 +12,14 @@ public class Bek {
         System.out.println("hello, world");
 
         ApiClient client = Config.defaultClient();
+        Configuration.setDefaultApiClient(client);
+
+        ProtoClient pc = new ProtoClient(client);
+        ObjectOrStatus<V1.PodList> list = pc.list(V1.PodList.newBuilder(), "/api/v1/namespaces/default/pods");
+
+        if (list.object.getItemsCount() > 0) {
+            V1.Pod p = list.object.getItems(0);
+            System.out.println(p);
+        }
     }
 }
