4650889800568464ed3fba325091a230b526415d
Michael Vorburger <mike@vorburger.ch>
2023-02-20 15:26:44 +0100

    URI 1

--- core/lib/src/main/java/dev/enola/core/URIs.java
+++ core/lib/src/main/java/dev/enola/core/URIs.java
@@ -2,6 +2,31 @@ package dev.enola.core;
 
 import dev.enola.core.proto.URI;
 
-public class URIs {
-    URI proto;
+public final class URIs {
+    private URIs() { }
+
+    public static URI from(String s) {
+        java.net.URI uri = java.net.URI.create(s);
+        if (uri.getScheme() == null) {
+            throw new IllegalArgumentException(s + " URI has no scheme: " + uri);
+        }
+        if (uri.getAuthority() == null && uri.getSchemeSpecificPart() == null) {
+            throw new IllegalArgumentException(s + " URI has no authority: " + uri);
+        }
+        URI.Parts.Builder builder = URI.Parts.newBuilder().setScheme(uri.getScheme());
+        if (uri.getSchemeSpecificPart() != null)
+            builder.setAuthority(uri.getSchemeSpecificPart());
+
+        return URI.newBuilder().setParts(builder).build();
+    }
+
+    public static URI struct(URI uri) {
+        if (uri.hasParts()) {
+            return uri;
+        }
+        if (!uri.hasText()) {
+            throw new IllegalArgumentException("URI proto is empty and has neither text nor struct");
+        }
+        return from(uri.getText());
+    }
 }
--- core/lib/src/test/java/dev/enola/core/URIsTest.java
+++ core/lib/src/test/java/dev/enola/core/URIsTest.java
@@ -1,9 +1,17 @@
 package dev.enola.core;
 
+import dev.enola.core.proto.URI;
+import org.junit.Assert;
 import org.junit.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
 
 public class URIsTest {
     @Test
     public void testURI() {
+        assertThrows(IllegalArgumentException.class, () -> URIs.from("demo"));
+        assertThrows(IllegalArgumentException.class, () -> URIs.from("demo:"));
+        assertThat(URIs.from("demo:foo")).isEqualTo(URI.newBuilder().setParts(URI.Parts.newBuilder().setScheme("demo").setAuthority("foo")).build());
     }
 }
