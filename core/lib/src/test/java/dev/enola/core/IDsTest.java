8ef9e5183ca418d9d6fa1eb493f3e409b25b51ee
Michael Vorburger <mike@vorburger.ch>
2023-02-20 18:14:26 +0100

    ID toString()

--- core/lib/src/test/java/dev/enola/core/IDsTest.java
+++ core/lib/src/test/java/dev/enola/core/IDsTest.java
@@ -7,20 +7,38 @@ import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.assertThrows;
 
 public class IDsTest {
-    @Test
-    public void testID() {
-        // Good
-        ID proto = ID.newBuilder().setParts(ID.Parts.newBuilder().setScheme("demo").setEntity("foo")).build();
-        assertThat(IDs.from("demo:foo")).isEqualTo(proto);
-        assertThat(IDs.from("demo:foo?")).isEqualTo(proto);
 
-        assertThat(IDs.from("k8s:pod?network=prod&context=demo&namespace=test&name=hello")).isEqualTo(
-            ID.newBuilder().setParts(ID.Parts.newBuilder().setScheme("k8s").setEntity("pod")
-                .addQuery(ID.NameValue.newBuilder().setName("network").setValue("prod"))
-                .addQuery(ID.NameValue.newBuilder().setName("context").setValue("demo"))
-                .addQuery(ID.NameValue.newBuilder().setName("namespace").setValue("test"))
-                .addQuery(ID.NameValue.newBuilder().setName("name").setValue("hello"))
-            ).build());
+    String uri1 = "demo:foo";
+    ID proto1 = ID.newBuilder().setParts(ID.Parts.newBuilder().setScheme("demo").setEntity("foo")).build();
+    ID proto1text = ID.newBuilder().setText(uri1).build();
+
+    String uri2 = "k8s:pod?network=prod&context=demo&namespace=test&name=hello";
+    ID proto2 = ID.newBuilder().setParts(ID.Parts.newBuilder().setScheme("k8s").setEntity("pod")
+            .addQuery(ID.NameValue.newBuilder().setName("network").setValue("prod"))
+            .addQuery(ID.NameValue.newBuilder().setName("context").setValue("demo"))
+            .addQuery(ID.NameValue.newBuilder().setName("namespace").setValue("test"))
+            .addQuery(ID.NameValue.newBuilder().setName("name").setValue("hello"))
+    ).build();
+
+    @Test
+    public void testParts() {
+        assertThat(IDs.parts(proto1)).isEqualTo(proto1.getParts());
+        assertThat(IDs.parts(proto1text)).isEqualTo(proto1.getParts());
+    }
+
+    @Test
+    public void testToString() {
+        assertThat(IDs.toString(proto1)).isEqualTo(uri1);
+        assertThat(IDs.toString(proto1text)).isEqualTo(uri1);
+        assertThat(IDs.toString(proto2)).isEqualTo(uri2);
+    }
+
+    @Test
+    public void testURI() {
+        // Good
+        assertThat(IDs.from(uri1)).isEqualTo(proto1);
+        assertThat(IDs.from(uri1 + "?")).isEqualTo(proto1);
+        assertThat(IDs.from(uri2)).isEqualTo(proto2);
 
         // Bad
         badEnolaID("|");                    // Illegal Character
