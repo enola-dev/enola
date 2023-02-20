6aa2305084c09e3449d45f465aecfd9db13f95ba
Michael Vorburger <mike@vorburger.ch>
2023-02-20 17:49:50 +0100

    ID 4

--- core/lib/src/main/java/dev/enola/core/IDs.java
+++ core/lib/src/main/java/dev/enola/core/IDs.java
@@ -1,31 +1,67 @@
 package dev.enola.core;
 
+import com.google.common.base.Splitter;
+import com.google.common.collect.Iterables;
 import dev.enola.core.proto.ID;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public final class IDs {
     private IDs() { }
+
+    private final static Splitter AMPERSAND_SPLITTER = Splitter.on('&').omitEmptyStrings().trimResults();
+    private final static Splitter EQUALSIGN_SPLITTER = Splitter.on('=').omitEmptyStrings().trimResults();
 
     public static ID from(String s) {
         java.net.URI uri = java.net.URI.create(s);
         ID.Parts.Builder builder = ID.Parts.newBuilder();
 
         if (uri.getScheme() == null) {
-            throw new IllegalArgumentException(s + " ID has no scheme: " + uri);
+            throw new IllegalArgumentException(s + " ID URI has no scheme: " + uri);
         }
         builder.setScheme(uri.getScheme());
 
         if (uri.getAuthority() != null) {
-            throw new IllegalArgumentException(s + " ID cannot have an //authority: " + uri);
+            throw new IllegalArgumentException(s + " ID URI cannot have an //authority: " + uri);
         }
 
         if (uri.getFragment() != null) {
-            throw new IllegalArgumentException(s + " ID cannot have an #fragment: " + uri);
+            throw new IllegalArgumentException(s + " ID URI cannot have an #fragment: " + uri);
         }
 
         if (uri.getSchemeSpecificPart() == null) {
-            throw new IllegalArgumentException(s + " ID has no authority: " + uri);
+            throw new IllegalArgumentException(s + " ID URI has no path: " + uri);
         }
-        builder.setEntity(uri.getSchemeSpecificPart());
+        String path = uri.getSchemeSpecificPart();
+        int idx = path.indexOf('?');
+        if (idx == -1)
+            builder.setEntity(uri.getSchemeSpecificPart());
+        else {
+            String entity = path.substring(0, idx);
+            builder.setEntity(entity);
+
+            Set<String> queryParameterNames = new HashSet<>();
+            String query = path.substring(idx+1);
+            AMPERSAND_SPLITTER.split(query).forEach(pair -> {
+                String[] nameValue = Iterables.toArray(EQUALSIGN_SPLITTER.split(pair), String.class);
+                if (nameValue.length > 2) {
+                    throw new IllegalArgumentException(s + " ID URI ?query has name/value with more than 1 '=' sign: " + pair);
+                }
+                if (nameValue.length > 1) {
+                    if (queryParameterNames.contains(nameValue[0]))
+                        throw new IllegalArgumentException(s + " ID URI ?query has duplicate names: " + query);
+                    queryParameterNames.add(nameValue[0]);
+
+                    ID.NameValue.Builder nameValueBuilder = ID.NameValue.newBuilder();
+                    nameValueBuilder.setName(nameValue[0]);
+                    if (nameValue.length == 2) {
+                        nameValueBuilder.setValue(nameValue[1]);
+                    }
+                    builder.addQuery(nameValueBuilder);
+                }
+            });
+        }
 
         return ID.newBuilder().setParts(builder).build();
     }
@@ -35,7 +71,7 @@ public final class IDs {
             return id;
         }
         if (!id.hasText()) {
-            throw new IllegalArgumentException("ID proto is empty and has neither text nor struct");
+            throw new IllegalArgumentException("ID proto is empty and has neither text nor parts oneof");
         }
         return from(id.getText());
     }
--- core/lib/src/test/java/dev/enola/core/IDsTest.java
+++ core/lib/src/test/java/dev/enola/core/IDsTest.java
@@ -10,18 +10,28 @@ public class IDsTest {
     @Test
     public void testID() {
         // Good
-        assertThat(IDs.from("demo:foo")).isEqualTo(ID.newBuilder().setParts(ID.Parts.newBuilder().setScheme("demo").setEntity("foo")).build());
-        // TODO Use https://truth.dev/protobufs.html to assert object
-        assertThat(IDs.from("k8s:pod?network=prod&context=demo&namespace=test&name=hello")).isNotNull();
+        ID proto = ID.newBuilder().setParts(ID.Parts.newBuilder().setScheme("demo").setEntity("foo")).build();
+        assertThat(IDs.from("demo:foo")).isEqualTo(proto);
+        assertThat(IDs.from("demo:foo?")).isEqualTo(proto);
+
+        assertThat(IDs.from("k8s:pod?network=prod&context=demo&namespace=test&name=hello")).isEqualTo(
+            ID.newBuilder().setParts(ID.Parts.newBuilder().setScheme("k8s").setEntity("pod")
+                .addQuery(ID.NameValue.newBuilder().setName("network").setValue("prod"))
+                .addQuery(ID.NameValue.newBuilder().setName("context").setValue("demo"))
+                .addQuery(ID.NameValue.newBuilder().setName("namespace").setValue("test"))
+                .addQuery(ID.NameValue.newBuilder().setName("name").setValue("hello"))
+            ).build());
 
         // Bad
-        badEnolaID("|");                 // Illegal Character
-        badEnolaID("demo:fo|o");         // Still illegal Character
-        badEnolaID("demo");              // Path required
-        badEnolaID("demo:");             // Still Path required
-        badEnolaID("demo://foo");        // No "authority"
-        badEnolaID("demo://foo/path");   // Still no "authority"
-        badEnolaID("demo:foo#fragment"); // No Fragments (?)
+        badEnolaID("|");                    // Illegal Character
+        badEnolaID("demo:fo|o");            // Still illegal Character
+        badEnolaID("demo");                 // Path required
+        badEnolaID("demo:");                // Still Path required
+        badEnolaID("demo://foo");           // No "authority"
+        badEnolaID("demo://foo/path");      // Still no "authority"
+        badEnolaID("demo:foo#fragment");    // No Fragments (?)
+        badEnolaID("demo:foo?bad=a=b");     // Nope, this is confusing
+        badEnolaID("demo:foo?bad=a&bad=b"); // Duplicate query names
     }
 
     private void badEnolaID(String id) {
