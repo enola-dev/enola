d181c90ab67b7d8081229b33efdbcc8360da47a3
Michael Vorburger <mike@vorburger.ch>
2023-02-20 15:50:27 +0100

    URI 2

--- core/lib/src/test/java/dev/enola/core/URIsTest.java
+++ core/lib/src/test/java/dev/enola/core/URIsTest.java
@@ -10,8 +10,12 @@ import static org.junit.Assert.assertThrows;
 public class URIsTest {
     @Test
     public void testURI() {
+        // Bad
+        assertThrows(IllegalArgumentException.class, () -> URIs.from("|"));
         assertThrows(IllegalArgumentException.class, () -> URIs.from("demo"));
         assertThrows(IllegalArgumentException.class, () -> URIs.from("demo:"));
+
+        // Good
         assertThat(URIs.from("demo:foo")).isEqualTo(URI.newBuilder().setParts(URI.Parts.newBuilder().setScheme("demo").setEntity("foo")).build());
     }
 }
