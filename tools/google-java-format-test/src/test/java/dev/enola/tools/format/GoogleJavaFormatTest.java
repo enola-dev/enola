2d9d1e8cbaed75091a7349c0dd587b96f857c0e4
Michael Vorburger <vorburger@google.com>
2023-03-10 20:05:02 +0100

    WIP

--- tools/google-java-format-test/src/test/java/dev/enola/tools/format/GoogleJavaFormatTest.java
+++ tools/google-java-format-test/src/test/java/dev/enola/tools/format/GoogleJavaFormatTest.java
@@ -1,10 +1,9 @@
 package dev.enola.tools.format;
 
-import com.google.googlejavaformat.java.JavaFormatterOptions;
-import com.google.googlejavaformat.java.Main;
+import com.google.googlejavaformat.java.GoogleJavaFormatToolProvider;
+import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
@@ -13,17 +12,8 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 public class GoogleJavaFormatTest {
     @Test
     public void testGoogleJavaFormat() {
-
-        JavaFormatterOptions options =
-                JavaFormatterOptions.builder()
-                        .style(JavaFormatterOptions.Style.GOOGLE)
-                        .formatJavadoc(true)
-                        .reorderModifiers(true)
-                        .build();
-
         PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out, UTF_8));
         PrintWriter err = new PrintWriter(new OutputStreamWriter(System.err, UTF_8));
-        InputStream in = InputStream.nullInputStream();
-        new Main(out, err, in).format("GoogleJavaFormatTest.java");
+        Assert.assertEquals(0, new GoogleJavaFormatToolProvider().run(out, err, "--help"));
     }
 }
