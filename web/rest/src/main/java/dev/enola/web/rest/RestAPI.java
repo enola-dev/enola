55ff7f6f41a03d112b8d88256b83e5a2e57fad79
Michael Vorburger <mike@vorburger.ch>
2023-05-28 08:37:32 +0200

    feat  (web): Introduce JSON REST API

--- web/rest/src/main/java/dev/enola/web/rest/RestAPI.java
+++ web/rest/src/main/java/dev/enola/web/rest/RestAPI.java
@@ -15,80 +15,69 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package dev.enola.web.ui;
+package dev.enola.web.rest;
 
-import com.google.common.collect.ImmutableMap;
 import com.google.common.net.MediaType;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 
-import dev.enola.common.io.resource.ClasspathResource;
+import dev.enola.common.io.resource.MemoryResource;
 import dev.enola.common.io.resource.ReadableResource;
-import dev.enola.common.io.resource.StringResource;
+import dev.enola.common.io.resource.WritableResource;
+import dev.enola.common.protobuf.ProtoIO;
 import dev.enola.core.EnolaException;
 import dev.enola.core.EnolaService;
 import dev.enola.core.IDs;
-import dev.enola.core.proto.Entity;
 import dev.enola.core.proto.GetEntityRequest;
 import dev.enola.core.proto.ID;
-import dev.enola.web.StaticWebHandler;
 import dev.enola.web.WebHandler;
 import dev.enola.web.WebServer;
 
 import java.io.IOException;
 import java.net.URI;
-import java.util.Map;
 
-public class UI implements WebHandler {
+public class RestAPI implements WebHandler {
 
-    private final Soy soy =
-            new Soy.Builder()
-                    .addSoy("ui.soy")
-                    .addProto(ID.getDescriptor(), Entity.getDescriptor())
-                    .build();
-
+    private final ProtoIO protoIO = new ProtoIO();
     private final EnolaService service;
 
-    public UI(EnolaService service) {
+    public RestAPI(EnolaService service) {
         this.service = service;
     }
 
     public void register(WebServer server) {
-        server.register("/ui", this);
-        server.register("/ui/static/", new StaticWebHandler("/ui/static/", "static"));
+        server.register("/api", this);
     }
 
     @Override
     public ListenableFuture<ReadableResource> get(URI uri) {
         try {
-            String html = getHTML(uri);
-            var resource = new StringResource(html, MediaType.HTML_UTF_8);
+            var resource = new MemoryResource(MediaType.JSON_UTF_8);
+            writeJSON(uri, resource);
             return Futures.immediateFuture(resource);
         } catch (EnolaException | IOException e) {
             return Futures.immediateFailedFuture(e);
         }
     }
 
-    private String getHTML(URI uri) throws EnolaException, IOException {
+    private void writeJSON(URI uri, WritableResource resource) throws EnolaException, IOException {
         var path = uri.getPath();
-        if (path.startsWith("/ui/entity/")) {
-            var idString = path.substring("/ui/entity/".length());
+        if (path.startsWith("/api/entity/")) {
+            var idString = path.substring("/api/entity/".length());
             var id = IDs.parse(idString);
-            return getEntityHTML(id);
+            getEntityJSON(id, resource);
         } else {
-            // TODO Create HTML page “frame” from .soy, with body from another .soy
-            return new ClasspathResource("404.html").charSource().read();
+            // TODO 404 instead 500 (needs API changes)
+            throw new IllegalArgumentException("404 - Unknown URI!");
         }
     }
 
-    private String getEntityHTML(ID id) throws EnolaException {
+    private void getEntityJSON(ID id, WritableResource resource)
+            throws EnolaException, IOException {
         var request = GetEntityRequest.newBuilder().setId(id).build();
         var response = service.getEntity(request);
         var entity = response.getEntity();
 
-        Map<String, ?> params = ImmutableMap.of("e", entity);
-        var renderer = soy.newRenderer("dev.enola.ui.page", params);
-        // TODO Make this async - but I don't understand Soy's async API...
-        return renderer.renderHtml().get().getContent();
+        protoIO.write(entity, resource);
     }
 }
