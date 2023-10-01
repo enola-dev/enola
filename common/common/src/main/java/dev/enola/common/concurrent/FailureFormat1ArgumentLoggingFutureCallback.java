d72c8428742525f790d15a7e8e3a562ad6b67ccb
Michael Vorburger <mike@vorburger.ch>
2023-10-01 23:10:21 +0200

    chore(deps): Fixed Copyright to pass pre-commit and updated @author for clarity of origin

--- common/common/src/main/java/dev/enola/common/concurrent/FailureFormat1ArgumentLoggingFutureCallback.java
+++ common/common/src/main/java/dev/enola/common/concurrent/FailureFormat1ArgumentLoggingFutureCallback.java
@@ -1,9 +1,19 @@
 /*
- * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
  *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * Copyright 2017 - 2023 The Enola <https://enola.dev> Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package dev.enola.common.concurrent;
 
@@ -14,7 +24,7 @@ import org.slf4j.Logger;
 /**
  * Failure logging future callback with a message format String and 1 argument.
  *
- * @author Michael Vorburger.ch
+ * @author Michael Vorburger.ch, originally for https://www.opendaylight.org
  */
 // package-local not public (for the time being)
 final class FailureFormat1ArgumentLoggingFutureCallback<V>
