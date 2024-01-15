5be13f44470d05638abaf0df9e2d5b00c31cd3e9
Michael Vorburger <mike@vorburger.ch>
2024-01-15 19:58:14 +0100

    fix  (deps): Always run pip install on  (because tools/be/pip-installed.bash is actually broken)

--- tools/be/pip-installed.bash
+++ tools/be/pip-installed.bash
@@ -17,19 +17,24 @@
 
 # This runs "pip install", but (only) if requirements.txt has changed.
 
+# TODO Cute idea, but this doesn't actually work, like this... fix!
+
 # TODO Use sha512sum (or b2sum) instead of md5sum!
 # TODO Use cksum <https://man7.org/linux/man-pages/man1/cksum.1.html> instead, with parameter?
 # TODO Replace this with Enola's very own Resource hash() and exec() ...
 
 HASH_FILE=.pip-install.hash.txt
 
-if ! md5sum --warn --status --check $HASH_FILE ; then
+# Always run on CI (because THIS IS BROKEN!)
+if ! md5sum --warn --status --check $HASH_FILE || [ -n "$CI" ]; then
   echo
   set -e
   pip install -r requirements.txt
   md5sum --tag requirements.txt >$HASH_FILE
   # Technically not required, but let's just double check:
   md5sum --warn --status --strict --check $HASH_FILE
+else
+  echo "Skipping running pip install - BUT THIS DETECTION IS BROKEN ANYWAY AS-IS! :("
 fi
 
 # PS: A "real" (non-script) implementation should hash the command itself as well.
