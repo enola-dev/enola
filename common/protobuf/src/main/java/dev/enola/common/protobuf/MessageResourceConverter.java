a463431cd56b692bb84a6bea9065b8c828acbfd2
Michael Vorburger <mike@vorburger.ch>
2024-02-17 14:12:02 +0100

    fix  (core): Introduce MessageResourceConverter, and use in Rosetta #refactor

---
+++ common/protobuf/src/main/java/dev/enola/common/protobuf/MessageResourceConverter.java
@@ -0,0 +1,52 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2024 The Enola <https://enola.dev> Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.enola.common.protobuf;
+
+import com.google.protobuf.Descriptors;
+import com.google.protobuf.DynamicMessage;
+
+import dev.enola.common.io.resource.ReadableResource;
+import dev.enola.common.io.resource.WritableResource;
+import dev.enola.common.io.resource.convert.ResourceConverter;
+
+import java.io.IOException;
+
+public class MessageResourceConverter implements ResourceConverter {
+
+    private final DescriptorProvider descriptorProvider;
+    private final ProtoIO protoIO;
+
+    public MessageResourceConverter(ProtoIO protoIO, DescriptorProvider descriptorProvider) {
+        this.descriptorProvider = descriptorProvider;
+        this.protoIO = protoIO;
+    }
+
+    @Override
+    public boolean convertInto(ReadableResource from, WritableResource into) throws IOException {
+        var fromProtoFQN = ProtobufMediaTypes.getProtoMessageFQN(from.mediaType());
+        if (fromProtoFQN.isPresent()) {
+            Descriptors.Descriptor descriptor = descriptorProvider.findByName(fromProtoFQN.get());
+            // TODO Use new Messages (EnolaMessages) utility here #performance
+            var builder = DynamicMessage.newBuilder(descriptor);
+            protoIO.convert(from, builder, into);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
