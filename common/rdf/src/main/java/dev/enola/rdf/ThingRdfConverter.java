cb08759b453f8e73f0b751029e28ab227c2ea5bf
Michael Vorburger <mike@vorburger.ch>
2024-02-24 12:56:18 +0100

    fix  (core): Merge ThingRdfConverter & ThingRdfConverterInto

--- common/rdf/src/main/java/dev/enola/rdf/ThingRdfConverter.java
+++ common/rdf/src/main/java/dev/enola/rdf/ThingRdfConverter.java
@@ -19,21 +19,79 @@ package dev.enola.rdf;
 
 import dev.enola.common.convert.ConversionException;
 import dev.enola.common.convert.Converter;
+import dev.enola.common.convert.ConverterInto;
 import dev.enola.thing.ThingOrBuilder;
+import dev.enola.thing.Value.LangString;
 
+import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.model.impl.ValidatingValueFactory;
 import org.eclipse.rdf4j.model.util.ModelBuilder;
+import org.eclipse.rdf4j.rio.RDFHandler;
 import org.eclipse.rdf4j.rio.helpers.StatementCollector;
 
-class ThingRdfConverter implements Converter<ThingOrBuilder, Model> {
+class ThingRdfConverter
+        implements Converter<ThingOrBuilder, Model>, ConverterInto<ThingOrBuilder, RDFHandler> {
 
-    private final ThingRdfConverterInto converterInto = new ThingRdfConverterInto();
+    private final ValueFactory vf;
+
+    public ThingRdfConverter(ValueFactory vf) {
+        this.vf = vf;
+    }
+
+    public ThingRdfConverter() {
+        // just like in Values.VALUE_FACTORY
+        this(new ValidatingValueFactory(SimpleValueFactory.getInstance()));
+    }
 
     @Override
     public Model convert(ThingOrBuilder input) throws ConversionException {
         var model = new ModelBuilder().build();
         var statementCollector = new StatementCollector(model);
-        converterInto.convertInto(input, statementCollector);
+        convertInto(input, statementCollector);
         return model;
+    }
+
+    @Override
+    public boolean convertInto(ThingOrBuilder from, RDFHandler into) throws ConversionException {
+        into.startRDF();
+        IRI subject = vf.createIRI(from.getIri());
+
+        for (var field : from.getFieldsMap().entrySet()) {
+            IRI predicate = vf.createIRI(field.getKey());
+            var object = convert(field.getValue());
+            Statement statement = vf.createStatement(subject, predicate, object);
+            into.handleStatement(statement);
+        }
+
+        into.endRDF();
+        return true;
+    }
+
+    private org.eclipse.rdf4j.model.Value convert(dev.enola.thing.Value value) {
+        return switch (value.getKindCase()) {
+            case LINK -> vf.createIRI(value.getLink().getIri());
+
+            case STRING -> vf.createLiteral(value.getString());
+
+            case LITERAL -> {
+                var literal = value.getLiteral();
+                yield vf.createLiteral(literal.getValue(), vf.createIRI(literal.getDatatype()));
+            }
+
+            case LANG_STRING -> {
+                LangString langString = value.getLangString();
+                yield vf.createLiteral(langString.getText(), langString.getLang());
+            }
+
+            case LIST -> throw new UnsupportedOperationException("TODO");
+
+            case STRUCT -> throw new UnsupportedOperationException("TODO");
+
+            case KIND_NOT_SET -> throw new IllegalArgumentException(value.toString());
+        };
     }
 }
