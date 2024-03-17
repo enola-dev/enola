086c8b50ab5e21e425cb51ea2e471eb959c32a35
Michael Vorburger <mike@vorburger.ch>
2024-03-17 23:54:26 +0100

    feat  (cli): enola info extensions & detect

---
+++ cli/src/main/java/dev/enola/cli/DetectCommand.java
@@ -0,0 +1,48 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2024 The Enola <https://enola.dev> Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.enola.cli;
+
+import dev.enola.common.io.mediatype.MediaTypeProviders;
+import dev.enola.common.io.resource.ResourceProviders;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Model.CommandSpec;
+import picocli.CommandLine.Spec;
+
+import java.net.URI;
+
+@Command(
+        name = "detect",
+        description = "Provides information about the media type detected for a given IRI")
+public class DetectCommand implements CheckedRunnable {
+
+    @Spec CommandSpec spec;
+
+    @CommandLine.Parameters(index = "0", paramLabel = "iri", description = "IRI")
+    String iri;
+
+    @Override
+    public void run() throws Exception {
+        var resource = new ResourceProviders().getResource(new URI(iri));
+        var mediaType = MediaTypeProviders.SINGLETON.detect(resource).orElse(resource.mediaType());
+
+        var pw = spec.commandLine().getOut();
+        pw.println(mediaType);
+    }
+}
