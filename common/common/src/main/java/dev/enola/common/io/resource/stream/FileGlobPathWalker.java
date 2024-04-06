8df40d3d7c8fe4f59bdbcb44b7885a156ef59777
Michael Vorburger <mike@vorburger.ch>
2024-04-06 19:21:00 +0200

    fix  (core): Add initial Test Coverage for, and fix, the new FileGlobResourceProvider

---
+++ common/common/src/main/java/dev/enola/common/io/resource/stream/FileGlobPathWalker.java
@@ -0,0 +1,46 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2024 The Enola <https://enola.dev> Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.enola.common.io.resource.stream;
+
+import java.io.IOException;
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+/** FileGlobPathWalker is a utility to enumerate files matching a glob pattern. */
+final class FileGlobPathWalker {
+
+    /** See the {@link FileGlobResourceProvider} documentation for glob path description. */
+    static Stream<Path> walk(Path globPath) throws IOException {
+        var globString = globPath.toString();
+        var starPos = globString.indexOf('*');
+        if (starPos == -1)
+            throw new IllegalArgumentException(
+                    "TODO: Implement missing support for non-* glob: " + globString);
+        var basePath = Path.of(globString.substring(0, starPos - 1));
+
+        // Inspired by File.newDirectoryStream(), but matching full path, not just getFileName()
+        var fs = basePath.getFileSystem();
+        var matcher = fs.getPathMatcher("glob:" + globString);
+        return Files.walk(basePath, FileVisitOption.FOLLOW_LINKS)
+                .filter(path -> matcher.matches(path));
+    }
+
+    private FileGlobPathWalker() {}
+}
---
+++ common/common/src/test/java/dev/enola/common/io/resource/stream/FileGlobResourceProviderTest.java
@@ -0,0 +1,77 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2024 The Enola <https://enola.dev> Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.enola.common.io.resource.stream;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class FileGlobResourceProviderTest {
+
+    @ClassRule public static TemporaryFolder tempFolder = new TemporaryFolder();
+
+    @BeforeClass
+    public static void beforeClass() throws IOException {
+        var tempRoot = tempFolder.getRoot();
+        Files.createFile(new File(tempRoot, "a.txt").toPath());
+        Files.createFile(new File(tempRoot, "b.txt").toPath());
+        Files.createFile(new File(tempRoot, "c.json").toPath());
+        Files.createFile(new File(tempRoot, "d.yaml").toPath());
+
+        Files.createDirectory(new File(tempRoot, "subdir/").toPath());
+        Files.createFile(new File(tempRoot, "subdir/e.txt").toPath());
+    }
+
+    private void check(String suffix, int expectedFiles) {
+        var globIRI = tempFolder.getRoot().getAbsoluteFile().toURI().toString() + "/" + suffix;
+        try (var stream = new FileGlobResourceProvider().get(globIRI)) {
+            assertThat(stream).hasSize(expectedFiles);
+        }
+    }
+
+    @Test
+    public void globStarTXT() {
+        check("*.txt", 2);
+    }
+
+    @Test
+    public void globStarStarSlashStarTXT() {
+        // **/*.txt only matches sub-dirs...
+        check("**/*.txt", 1);
+    }
+
+    @Test
+    public void globStarStarTXT() {
+        // **.txt matches TXT in root and all sub-dirs...
+        check("**.txt", 3);
+    }
+
+    // TODO Test single file name, without * glob
+
+    // TODO Test non-* glob (e.g. {})
+
+    // TODO Test Directory name matching glob pattern should be excluded (as it's not a readable
+    // "resource" / file)
+}
