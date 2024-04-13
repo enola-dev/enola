38367473033e185ad80699accbdbce5193fd39b7
Michael Vorburger <mike@vorburger.ch>
2024-04-13 18:40:18 +0200

    feat (core): Sneaky 👟 MoreStreams.forEach() - whoa, neat!

---
+++ common/common/src/main/java/dev/enola/common/function/MoreStreams.java
@@ -0,0 +1,82 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2024 The Enola <https://enola.dev> Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.enola.common.function;
+
+import com.google.common.base.Throwables;
+import com.google.common.collect.Streams;
+
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Stream;
+
+/** Static utility methods related to {@code Stream} instances. {@link Streams} has more. */
+public final class MoreStreams {
+
+    // TODO Eventually adopting one of (but which?) real reactive frameworks in Enola overall
+    // and rm this may be better? (That would likely be better than doing something such as e.g.
+    // https://stackoverflow.com/questions/30117134/aggregate-runtime-exceptions-in-java-8-streams)
+
+    // While waiting for e.g. something like https://bugs.openjdk.org/browse/JDK-8148917
+    public static <T, E extends Exception> void forEach(
+            Stream<T> stream, CheckedConsumer<T, E> action) throws E {
+        if (stream.isParallel()) {
+            sneakyForEach(stream, action);
+        } else {
+            forEachInSeq(stream, action);
+        }
+    }
+
+    // This (probably, not verified) loose parallelism, if the stream even has it?
+    private static <T, E extends Exception> void forEachInSeq(
+            Stream<T> stream, CheckedConsumer<T, E> action) throws E {
+        // https://stackoverflow.com/questions/20129762/why-does-streamt-not-implement-iterablet/20130475#20130475
+        var iterator = stream.iterator();
+        while (iterator.hasNext()) {
+            T element = iterator.next();
+            action.accept(element);
+        }
+    }
+
+    private static <T, E extends Exception> void sneakyForEach(
+            Stream<T> stream, CheckedConsumer<T, E> action) throws E {
+        stream.forEach(Sneaker.sneakyConsumer(action));
+    }
+
+    // This was an initial attempt, which I then improved as above; keeping here only for history...
+    @SuppressWarnings("unchecked") // only for lastProblem.set((E) e)
+    private static <T, E extends Exception> void forEachThrowLast(
+            Stream<T> stream, CheckedConsumer<T, E> action) throws E {
+
+        AtomicReference<E> lastProblemReference = new AtomicReference<>();
+        stream.forEach(
+                element -> {
+                    try {
+                        action.accept(element);
+                    } catch (Exception e) {
+                        Throwables.throwIfUnchecked(e);
+                        // If we're still here, it was a checked Exception of E...
+                        lastProblemReference.set((E) e);
+                    }
+                });
+        E lastProblem = lastProblemReference.get();
+        if (lastProblem != null) {
+            throw lastProblem;
+        }
+    }
+
+    private MoreStreams() {}
+}
---
+++ common/common/src/main/java/dev/enola/common/function/Sneaker.java
@@ -0,0 +1,53 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2024 The Enola <https://enola.dev> Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.enola.common.function;
+
+import java.util.function.Consumer;
+
+/** 👟 */
+// This class
+// See e.g. is intentionally only package local, and only used via MoreStreams.
+// E.g. https://4comprehension.com/sneakily-throwing-exceptions-in-lambda-expressions-in-java/ and
+// https://www.baeldung.com/java-lambda-exceptions, but also
+// https://github.com/google/guava/wiki/Why-we-deprecated-Throwables.propagate, have some related
+// background. Contrary to what those and similar posts state, the way this is done is actually
+// entirely "safe" in Java... note that the trick here, and important difference to a general 👟
+// sneaker, is that the MoreStreams#forEach signature actually declares the throws E - so it's safe!
+final class Sneaker {
+
+    @SuppressWarnings("unchecked")
+    // This method should be kept private and never made public.
+    // Sneaky throwing in general is not a good idea, but
+    private static <T extends Exception, R> R sneakyThrow(Exception t) throws T {
+        throw (T) t; // ( ͡° ͜ʖ ͡°)
+    }
+
+    // This method is intentionally only package local, as is this entire class.
+    static <T, E extends Exception> Consumer<T> sneakyConsumer(
+            CheckedConsumer<T, E> checkedConsumer) {
+        return t -> {
+            try {
+                checkedConsumer.accept(t);
+            } catch (Exception e) {
+                sneakyThrow(e);
+            }
+        };
+    }
+
+    private Sneaker() {}
+}
