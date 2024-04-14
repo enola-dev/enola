9796c0bf620be6b6e2c6311bd13055821fdb43e7
Michael Vorburger <mike@vorburger.ch>
2024-04-14 16:56:49 +0200

    fix (build): Abort build in case of any broken symlinks

--- test.bash
+++ test.bash
@@ -20,6 +20,9 @@ set -euo pipefail
 # This script builds the project *WITHOUT* requiring containers.
 # It can be used *IN* a container though; and is so, by the ./build script.
 
+# Abort if there are any broken symlinks
+(find . -xtype l -ls | grep .) && exit 1
+
 # Similar also in the ./enola script:
 GO_BIN_PATH=$(go env GOPATH)/bin
 BZL=$GO_BIN_PATH/bazelisk
