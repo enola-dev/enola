80df2848be08ed577bdf164f9af400fdd4569bee
Michael Vorburger <mike@vorburger.ch>
2024-04-15 22:48:21 +0200

    fix (build): Big 🎂 Birthday Move, part II

---
+++ java/dev/enola/common/protobuf/ProtoIOTest.java
@@ -0,0 +1,190 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2023-2024 The Enola <https://enola.dev> Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.enola.common.protobuf;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import static org.junit.Assert.assertThrows;
+
+import com.google.common.net.MediaType;
+import com.google.protobuf.Any;
+import com.google.protobuf.Message;
+import com.google.protobuf.Timestamp;
+import com.google.protobuf.TypeRegistry;
+
+import dev.enola.common.io.resource.ClasspathResource;
+import dev.enola.common.io.resource.EmptyResource;
+import dev.enola.common.io.resource.MemoryResource;
+import dev.enola.common.io.resource.ReadableResource;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class ProtoIOTest {
+
+    public static final Timestamp TIMESTAMP =
+            Timestamp.newBuilder().setSeconds(123).setNanos(456).build();
+
+    private final String pathToResourceOnClasspath = "ok.textproto";
+    private final Message.Builder builder = Timestamp.newBuilder();
+
+    @Test
+    public void testProto() throws IOException {
+        ProtoIO.check(pathToResourceOnClasspath, builder);
+    }
+
+    private Timestamp readTimestamp(ReadableResource resource) throws IOException {
+        return new ProtoIO().read(resource, Timestamp.newBuilder(), Timestamp.class);
+    }
+
+    @Test
+    public void readGoodTextproto() throws IOException {
+        assertThat(readTimestamp(new ClasspathResource("ok.textproto"))).isEqualTo(TIMESTAMP);
+    }
+
+    @Test
+    public void readEmptyBinary() throws IOException {
+        var r = new EmptyResource(ProtobufMediaTypes.PROTOBUF_BINARY);
+        assertThat(readTimestamp(r)).isEqualTo(Timestamp.getDefaultInstance());
+    }
+
+    @Test
+    public void readEmptyTextproto() throws IOException {
+        var r = new EmptyResource(ProtobufMediaTypes.PROTOBUF_TEXTPROTO_UTF_8);
+        assertThat(readTimestamp(r)).isEqualTo(Timestamp.getDefaultInstance());
+    }
+
+    @Test
+    public void readEmptyJSON() throws IOException {
+        var r = new EmptyResource(ProtobufMediaTypes.PROTOBUF_JSON_UTF_8);
+        assertThat(readTimestamp(r)).isEqualTo(Timestamp.getDefaultInstance());
+    }
+
+    @Test
+    public void readEmptyYAML() throws IOException {
+        var r = new EmptyResource(ProtobufMediaTypes.PROTOBUF_YAML_UTF_8);
+        assertThat(readTimestamp(r)).isEqualTo(Timestamp.getDefaultInstance());
+    }
+
+    @Test
+    public void writeReadTextProto() throws IOException {
+        MemoryResource resource = new MemoryResource(ProtobufMediaTypes.PROTOBUF_TEXTPROTO_UTF_8);
+        new ProtoIO().write(TIMESTAMP, resource);
+        assertThat(resource.charSource().read()).isEqualTo("seconds: 123\nnanos: 456\n");
+
+        assertThat(new ProtoIO().read(resource, Timestamp.newBuilder(), Timestamp.class))
+                .isEqualTo(TIMESTAMP);
+    }
+
+    @Test
+    public void writeReadJSON() throws IOException {
+        MemoryResource resource = new MemoryResource(ProtobufMediaTypes.PROTOBUF_JSON_UTF_8);
+        new ProtoIO().write(TIMESTAMP, resource);
+        assertThat(resource.charSource().read()).isEqualTo("\"1970-01-01T00:02:03.000000456Z\"");
+
+        assertThat(new ProtoIO().read(resource, Timestamp.newBuilder(), Timestamp.class))
+                .isEqualTo(TIMESTAMP);
+    }
+
+    // TODO Add support for ProtobufMediaTypes.PROTOBUF_YAML_UTF_8
+
+    @Test
+    public void writeReadBinary() throws IOException {
+        MemoryResource resource = new MemoryResource(ProtobufMediaTypes.PROTOBUF_BINARY);
+        new ProtoIO().write(TIMESTAMP, resource);
+        assertThat(resource.byteSource().size()).isEqualTo(5);
+
+        assertThat(new ProtoIO().read(resource, Timestamp.newBuilder(), Timestamp.class))
+                .isEqualTo(TIMESTAMP);
+    }
+
+    @Test
+    public void writeUnknown() throws IOException {
+        MemoryResource resource = new MemoryResource(MediaType.ANY_TYPE);
+        assertThrows(IllegalStateException.class, () -> new ProtoIO().write(TIMESTAMP, resource));
+    }
+
+    @Test
+    public void readBadTextproto() throws IOException {
+        assertThat(
+                        assertThrows(
+                                ProtoIO.TextParseException.class,
+                                () ->
+                                        new ProtoIO()
+                                                .read(
+                                                        new ClasspathResource("nok.textproto"),
+                                                        Timestamp.newBuilder())))
+                .hasMessageThat()
+                .contains("google.protobuf.Timestamp.bad");
+
+        // new ProtoIO().merge(classpath("nok.textproto"), Timestamp.newBuilder());
+    }
+
+    @Test
+    public void writeReadAnyTextproto() throws IOException {
+        var typeRegistry = TypeRegistry.newBuilder().add(Timestamp.getDescriptor()).build();
+        var io = new ProtoIO(typeRegistry);
+        var any = Any.pack(TIMESTAMP);
+        var textproto =
+                """
+                [type.googleapis.com/google.protobuf.Timestamp] {
+                  seconds: 123
+                  nanos: 456
+                }
+                """;
+
+        var resource = new MemoryResource(ProtobufMediaTypes.PROTOBUF_TEXTPROTO_UTF_8);
+        io.write(any, resource);
+        assertThat(resource.charSource().read()).isEqualTo(textproto);
+
+        assertThat(io.read(resource, Any.newBuilder(), Any.class)).isEqualTo(any);
+    }
+
+    @Test
+    public void writeReadAnyJSON() throws IOException {
+        var typeRegistry = TypeRegistry.newBuilder().add(Timestamp.getDescriptor()).build();
+        var io = new ProtoIO(typeRegistry);
+        var any = Any.pack(TIMESTAMP);
+        var json =
+                "{\"@type\":\"type.googleapis.com/google.protobuf.Timestamp\","
+                        + "\"value\":\"1970-01-01T00:02:03.000000456Z\"}";
+
+        var resource = new MemoryResource(ProtobufMediaTypes.PROTOBUF_JSON_UTF_8);
+        io.write(any, resource);
+        assertThat(resource.charSource().read()).isEqualTo(json);
+
+        assertThat(io.read(resource, Any.newBuilder(), Any.class)).isEqualTo(any);
+    }
+
+    @Test
+    public void writeAnyYAML() throws IOException {
+        var typeRegistry = TypeRegistry.newBuilder().add(Timestamp.getDescriptor()).build();
+        var io = new ProtoIO(typeRegistry);
+        var any = Any.pack(TIMESTAMP);
+        var yaml =
+                "{'@type': type.googleapis.com/google.protobuf.Timestamp, value:"
+                        + " '1970-01-01T00:02:03.000000456Z'}\n";
+
+        var resource = new MemoryResource(ProtobufMediaTypes.PROTOBUF_YAML_UTF_8);
+        io.write(any, resource);
+        assertThat(resource.charSource().read()).isEqualTo(yaml);
+
+        assertThat(io.read(resource, Any.newBuilder(), Any.class)).isEqualTo(any);
+    }
+}
---
+++ java/dev/enola/core/meta/EntityKindRepository.java
@@ -0,0 +1,203 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2023-2024 The Enola <https://enola.dev> Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.enola.core.meta;
+
+import dev.enola.common.io.resource.ClasspathResource;
+import dev.enola.common.io.resource.ErrorResource;
+import dev.enola.common.io.resource.ReadableResource;
+import dev.enola.common.protobuf.MessageValidators;
+import dev.enola.common.protobuf.ProtoIO;
+import dev.enola.common.protobuf.ValidationException;
+import dev.enola.core.meta.proto.EntityKind;
+import dev.enola.core.meta.proto.EntityKinds;
+import dev.enola.core.proto.ID;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.TreeMap;
+
+public class EntityKindRepository {
+
+    private static final MessageValidators v = EntityKindValidations.INSTANCE;
+    private final Map<String, Map<String, CachedEntityKind>> map = new TreeMap<>();
+
+    public EntityKindRepository() {
+        try {
+            put(EntityKindAspect.ENTITY_KIND_ENTITY_KIND);
+            try {
+                load(new ClasspathResource("enola.dev/schema.textproto"));
+            } catch (IOException e) {
+                throw new IllegalStateException("Built-in ClasspathResource missing?!", e);
+            }
+        } catch (ValidationException e) {
+            // This cannot happen, because the built-in kinds are valid.
+            throw new IllegalStateException("BUG!", e);
+        }
+    }
+
+    public EntityKindRepository put(EntityKind entityKind) throws ValidationException {
+        var results = MessageValidators.Result.newBuilder();
+        put(entityKind, ErrorResource.INSTANCE, Optional.empty(), results);
+        validateFinal(results);
+        return this;
+    }
+
+    private synchronized void put(
+            EntityKind entityKind,
+            ReadableResource resource,
+            Optional<Instant> lastModified,
+            MessageValidators.Result.Builder r)
+            throws ValidationException {
+        var id = entityKind.getId();
+
+        // TODO Separately validating ID first should eventually not be required anymore,
+        // because Validation should "recurse into" messages by itself, later.
+        v.validate(id, r);
+        v.validate(entityKind, r);
+
+        map.computeIfAbsent(id.getNs(), s -> new TreeMap<>())
+                .put(id.getEntity(), new CachedEntityKind(entityKind, resource, lastModified));
+    }
+
+    public Optional<EntityKind> getOptional(ID id) {
+        var subMap = map.get(id.getNs());
+        if (subMap == null) {
+            return Optional.empty();
+        }
+        final var cachedEntityKind = subMap.get(id.getEntity());
+        if (cachedEntityKind == null) {
+            return Optional.empty();
+        }
+        final EntityKind[] finalEntityKind = new EntityKind[] {cachedEntityKind.entityKind};
+        cachedEntityKind.lastModified.ifPresent(
+                originalLastModified -> {
+                    cachedEntityKind
+                            .resource
+                            .lastModifiedIfKnown()
+                            .ifPresent(
+                                    currentLastModified -> {
+                                        if (currentLastModified.isAfter(originalLastModified)) {
+                                            try {
+                                                load(cachedEntityKind.resource);
+                                                finalEntityKind[0] =
+                                                        subMap.get(id.getEntity()).entityKind;
+                                            } catch (IOException | ValidationException e) {
+                                                throw new IllegalStateException(
+                                                        "Reload failed: "
+                                                                + cachedEntityKind.resource.uri(),
+                                                        e);
+                                            }
+                                        }
+                                    });
+                });
+        return Optional.of(finalEntityKind[0]);
+    }
+
+    public EntityKind get(ID id) {
+        return getOptional(id)
+                .orElseThrow(
+                        () -> new IllegalArgumentException(id + " unknown; available are: " + map));
+    }
+
+    public EntityKindRepository load(ReadableResource resource)
+            throws IOException, ValidationException {
+        return load(List.of(resource));
+    }
+
+    public EntityKindRepository load(Iterable<ReadableResource> resources)
+            throws IOException, ValidationException {
+        var results = MessageValidators.Result.newBuilder();
+        for (var resource : resources) {
+            var lastModified = resource.lastModifiedIfKnown();
+            var kinds = new ProtoIO().read(resource, EntityKinds.newBuilder()).build();
+            for (var kind : kinds.getKindsList()) {
+                put(kind, resource, lastModified, results);
+            }
+        }
+        validateFinal(results);
+        return this;
+    }
+
+    /**
+     * Final validation, for consistency of all models and the references across all of their {@link
+     * #load(ReadableResource)}-ed resources. Throws ValidationException if r has any problems.
+     */
+    private void validateFinal(MessageValidators.Result.Builder r) throws ValidationException {
+        var eks = EntityKinds.newBuilder();
+        list().forEach(ek -> eks.addKinds(ek));
+        v.validate(this, eks, r);
+        r.build().throwIt();
+    }
+
+    public Collection<EntityKind> list() {
+        var eks = new ArrayList<EntityKind>();
+        map.values()
+                .forEach(
+                        innerMap ->
+                                innerMap.values().stream()
+                                        .map(CachedEntityKind::entityKind)
+                                        .forEach(eks::add));
+        return eks;
+    }
+
+    public List<ID> listID() {
+        var ids = new ArrayList<ID>();
+        map.values()
+                .forEach(
+                        innerMap ->
+                                innerMap.values().stream()
+                                        .map(CachedEntityKind::entityKind)
+                                        .map(EntityKind::getId)
+                                        .forEach(ids::add));
+        return ids;
+    }
+
+    private static final class CachedEntityKind {
+        final EntityKind entityKind;
+        final ReadableResource resource;
+        final Optional<Instant> lastModified;
+
+        CachedEntityKind(
+                EntityKind entityKind, ReadableResource resource, Optional<Instant> lastModified) {
+            this.entityKind = entityKind;
+            this.resource = resource;
+            this.lastModified = lastModified;
+        }
+
+        EntityKind entityKind() {
+            return entityKind;
+        }
+
+        @Override
+        public String toString() {
+            return "CachedEntityKind{"
+                    + "entityKind="
+                    + entityKind
+                    + ", resource="
+                    + resource
+                    + ", lastModified="
+                    + lastModified
+                    + '}';
+        }
+    }
+}
