b617a0a6007c56e0992643e29e697c03364146b3
Michael Vorburger <mike@vorburger.ch>
2024-05-04 02:22:47 +0200

    clean(core): Move URITemplate* from core.iri to io.iri.template

--- java/dev/enola/common/io/iri/template/URITemplateTest.java
+++ java/dev/enola/common/io/iri/template/URITemplateTest.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package dev.enola.core.iri;
+package dev.enola.common.io.iri.template;
 
 import static com.google.common.truth.Truth.assertThat;
 
