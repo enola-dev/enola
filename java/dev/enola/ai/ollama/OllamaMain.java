144da9f03a51f48cacba1920926ade19a54b4ca3
Michael Vorburger <mike@vorburger.ch>
2024-05-20 03:19:28 +0200

    feat   (ai): The 🦙 can now suck in files by glob, e.g. **.java

--- java/dev/enola/ai/ollama/OllamaMain.java
+++ java/dev/enola/ai/ollama/OllamaMain.java
@@ -17,6 +17,9 @@
  */
 package dev.enola.ai.ollama;
 
+import dev.enola.common.function.MoreStreams;
+import dev.enola.common.io.resource.stream.GlobResourceProvider;
+import dev.enola.common.io.resource.stream.GlobResourceProviders;
 import io.github.amithkoujalgi.ollama4j.core.OllamaAPI;
 import io.github.amithkoujalgi.ollama4j.core.exceptions.OllamaBaseException;
 import io.github.amithkoujalgi.ollama4j.core.utils.OptionsBuilder;
@@ -30,25 +33,51 @@ public class OllamaMain {
 
     public static void main(String[] args)
             throws IOException, OllamaBaseException, URISyntaxException, InterruptedException {
-        if (args.length != 1) {
-            System.err.println("Usage: java OllamaMain <path-to-directory-of-code>");
+        // This won't work, because it's too big:
+        // args = new String[]
+        // {"file:/home/vorburger/git/github.com/enola-dev/enola/java/**.java?charset=UTF-8",
+        // "What's this code all about?"};
+        // ...
+        // But e.g. "file:$PWD/java/dev/enola/common/function/**.java?charset=UTF-8" kinda works!
 
-            var modelName = "gemma:7b"; // TODO "codegemma:7b"
-            var host = "http://localhost:11434/";
-            var options = new OptionsBuilder().setSeed(1).build();
-
-            OllamaAPI ollamaAPI = new OllamaAPI(host);
-            ollamaAPI.setRequestTimeoutSeconds(7);
-            ollamaAPI.ping();
-            // System.out.println(ollamaAPI.getModelDetails(modelName));
-            System.out.println(
-                    ollamaAPI
-                            .generate(
-                                    modelName,
-                                    "Hello, the sky is grey today. Hi, what's sky's colour? Do not"
-                                            + " provide any reasoning, just answer the question",
-                                    options)
-                            .getResponse());
+        if (args.length != 2) {
+            System.err.println("Usage: java OllamaMain <path-to-directory-of-code> <prompt>");
+            System.exit(1);
         }
+
+        var context = suckFiles(args[0]);
+        var prompt = args[1];
+
+        var modelName = "codegemma:7b";
+        var host = "http://localhost:11434/";
+        var options = new OptionsBuilder().setSeed(1).build();
+
+        OllamaAPI ollamaAPI = new OllamaAPI(host);
+        ollamaAPI.setRequestTimeoutSeconds(30);
+        ollamaAPI.ping();
+        // System.out.println(ollamaAPI.getModelDetails(modelName));
+
+        System.out.println("Context Size is: " + context.length());
+        System.out.println(
+                ollamaAPI
+                        .generate(
+                                modelName,
+                                "Here is a lot of source code: "
+                                        + context
+                                        + "\nNow please answer the following question about this code, and avoid repeating yourself: "
+                                        + prompt,
+                                // "Do not provide any reasoning, just answer the question",
+                                options)
+                        .getResponse());
+        System.out.println("Context Size was: " + context.length());
+    }
+
+    private static String suckFiles(String globURI) throws IOException {
+        var sb = new StringBuilder();
+        GlobResourceProvider globResourceProvider = new GlobResourceProviders();
+        try (var resources = globResourceProvider.get(globURI)) {
+            MoreStreams.forEach(resources, r -> sb.append(r.charSource().read()));
+        }
+        return sb.toString();
     }
 }
