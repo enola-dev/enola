9111c08292f99d48702e5208feb703828c8b983a
Michael Vorburger <mike@vorburger.ch>
2024-06-22 14:34:45 +0200

    fix  (core): Type Converters now correctly handle inputs of another Class Type

--- java/dev/enola/common/convert/ObjectToStringBiConverters.java
+++ java/dev/enola/common/convert/ObjectToStringBiConverters.java
@@ -17,8 +17,6 @@
  */
 package dev.enola.common.convert;
 
-import org.jspecify.annotations.Nullable;
-
 import java.net.URISyntaxException;
 import java.time.LocalDate;
 
@@ -30,88 +28,24 @@ public final class ObjectToStringBiConverters {
     public static final ObjectToStringBiConverter<String> STRING =
             new IdentityObjectToStringBiConverter();
 
-    // public static final ObjectToStringBiConverter<Boolean> BOOLEAN =
-    //        new TypeCheckingObjectStringWithToStringBiConverter<>(
-    //                Boolean.class, input -> input != null ? Boolean.valueOf(input) : null);
-
     public static final ObjectToStringBiConverter<Boolean> BOOLEAN =
-            new ObjectToStringToStringBiConverter<>() {
-
-                @Override
-                public Boolean convertFrom(@Nullable String input) throws ConversionException {
-                    return input != null ? Boolean.valueOf(input) : null;
-                }
-            };
+            new ObjectToStringWithToStringBiConverter<>(
+                    Boolean.class, input -> Boolean.valueOf(input));
 
     public static final ObjectToStringBiConverter<java.net.URI> URI =
-            new ObjectToStringToStringBiConverter<>() {
-
-                @Override
-                public java.net.URI convertFrom(@Nullable String input) throws ConversionException {
-                    try {
-                        return input != null ? new java.net.URI(input) : null;
-                    } catch (URISyntaxException e) {
-                        throw new ConversionException(input, e);
-                    }
-                }
-            };
+            new ObjectToStringWithToStringBiConverter<>(
+                    java.net.URI.class,
+                    input -> {
+                        try {
+                            return new java.net.URI(input);
+                        } catch (URISyntaxException e) {
+                            throw new ConversionException(input, e);
+                        }
+                    });
 
     public static final ObjectToStringBiConverter<LocalDate> LOCAL_DATE =
-            new ObjectToStringToStringBiConverter<>() {
-                @Override
-                public LocalDate convertFrom(@Nullable String input) throws ConversionException {
-                    return input != null ? LocalDate.parse(input) : null;
-                }
-            };
-
-    /**
-     * An ObjectToStringBiConverter which uses {@link Object#toString()} for {@link
-     * ObjectToStringBiConverter#convertTo(Object)}, and which also checks the type to convert from.
-     */
-    /*
-        private static class TypeCheckingObjectStringWithToStringBiConverter<T>
-                extends ObjectConverter<String, T> implements ObjectToStringBiConverter<T> {
-
-            public TypeCheckingObjectStringWithToStringBiConverter(
-                    Class<T> to, Function<String, T> converter) {
-                super(String.class, to, converter);
-            }
-
-            @Override
-            public @Nullable String convertTo(@Nullable T input) throws ConversionException {
-                return input != null ? input.toString() : null;
-            }
-
-            @Override
-            public @Nullable T convertFrom(@Nullable String input) throws ConversionException {
-                try {
-                    return convertFromType(input, to).orElse(null);
-                } catch (IOException e) {
-                    throw new ConversionException("Failed to convert: " + input, e);
-                }
-            }
-
-            // TODO @Override
-            public <T> Optional<T> convertFromType(String input, Class<T> type) throws IOException {
-                if (to.equals(type) && input != null && from.equals(input.getClass())) {
-                    return Optional.of((T) converter.apply((String) input));
-                }
-                return Optional.empty();
-            }
-        }
-    */
-
-    /**
-     * An ObjectToStringBiConverter which uses {@link Object#toString()} for {@link
-     * ObjectToStringBiConverter#convertTo(Object)}.
-     */
-    private abstract static class ObjectToStringToStringBiConverter<T>
-            implements ObjectToStringBiConverter<T> {
-        @Override
-        public final String convertTo(@Nullable T input) throws ConversionException {
-            return input != null ? input.toString() : null;
-        }
-    }
+            new ObjectToStringWithToStringBiConverter<>(
+                    LocalDate.class, input -> LocalDate.parse(input));
 
     private ObjectToStringBiConverters() {}
 }
