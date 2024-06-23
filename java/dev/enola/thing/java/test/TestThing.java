9eba8ec54bf369305a27d863d7985b34c218c625
Michael Vorburger <mike@vorburger.ch>
2024-06-23 17:32:59 +0200

    feat (core): Java ☕ Thing API with interfaces

---
+++ java/dev/enola/thing/java/test/TestThing.java
@@ -0,0 +1,43 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2024 The Enola <https://enola.dev> Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.enola.thing.java.test;
+
+import dev.enola.thing.KIRI;
+import dev.enola.thing.Thing;
+import dev.enola.thing.java.IRI;
+
+import org.jspecify.annotations.Nullable;
+
+public interface TestThing extends Thing {
+
+    // NB: This is only here like this for TestThingTest; otherwise this would be inlined in @IRI!
+    String NUMBER_URI = "https://enola.dev/test/number";
+
+    @IRI(NUMBER_URI)
+    @Nullable Integer number();
+
+    @IRI(KIRI.RDFS.LABEL)
+    @Nullable String label();
+
+    interface Builder<B extends TestThing> extends Thing.Builder<B> {
+
+        ImmutableTestThing.Builder label(String label);
+
+        ImmutableTestThing.Builder number(Integer number);
+    }
+}
