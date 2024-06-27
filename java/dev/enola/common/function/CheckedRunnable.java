4de3c2e9f07176da2115d7f777836b2bc2e4f53a
Michael Vorburger <mike@vorburger.ch>
2024-06-27 21:38:13 +0200

    clean (cli): Move CheckedRunnable to correct (newer) package

--- java/dev/enola/common/function/CheckedRunnable.java
+++ java/dev/enola/common/function/CheckedRunnable.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package dev.enola.cli;
+package dev.enola.common.function;
 
 import java.util.concurrent.Callable;
 
