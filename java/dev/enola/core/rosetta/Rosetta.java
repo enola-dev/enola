f21ca1130d3e8ad759421b9d67a797821afa6208
Michael Vorburger <mike@vorburger.ch>
2025-01-03 23:50:29 +0100

    clean(core): Remove TLC ResourceProvider.CTX

--- java/dev/enola/core/rosetta/Rosetta.java
+++ java/dev/enola/core/rosetta/Rosetta.java
@@ -1,7 +1,7 @@
 /*
  * SPDX-License-Identifier: Apache-2.0
  *
- * Copyright 2023-2024 The Enola <https://enola.dev> Authors
+ * Copyright 2023-2025 The Enola <https://enola.dev> Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -84,15 +84,10 @@ public class Rosetta implements ResourceConverter {
 
     private final ResourceConverterChain resourceConverterChain;
     private final ResourceProvider rp;
-    private final Loader loader; // TODO Remove!
-
-    public Rosetta(Loader loader) {
-        this(ResourceProvider.CTX, loader);
-    }
 
     public Rosetta(ResourceProvider rp, Loader loader) {
         this.rp = rp;
-        this.loader = loader;
+        // TODO Remove!
         var tmp = new ThingMetadataProvider(ThingProvider.CTX, NamespaceConverter.CTX);
         this.resourceConverterChain =
                 new ResourceConverterChain(
