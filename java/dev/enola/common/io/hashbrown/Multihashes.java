78b92af4a9e25932e87b0c51980d858cf54e75a6
Michael Vorburger <mike@vorburger.ch>
2025-01-04 06:17:08 +0100

    feat (core): Improve error message of new ?integrity=... generic URL query parameter

--- java/dev/enola/common/io/hashbrown/Multihashes.java
+++ java/dev/enola/common/io/hashbrown/Multihashes.java
@@ -29,14 +29,28 @@ public final class Multihashes {
     public static HashFunction toGuavaHashFunction(Multihash multihash) {
         var type = multihash.getType();
         return switch (type) {
-            case md5 -> Hashing.md5();
-            // TODO Add all other supported types...
+            // @Deprecated case md5 -> Hashing.md5();
+            // @Deprecated case sha1 -> Hashing.sha1();
+            case sha2_256 -> Hashing.sha256();
+            case sha2_512 -> Hashing.sha512();
+            // Not suitable, as it's tiny: case murmur3 -> Hashing.murmur3_32_fixed();
+
+            // TODO What about all other supported types?!
+            //   See https://github.com/multiformats/java-multihash/issues/41 ...
+
             default -> throw new IllegalArgumentException("Unsupported Multihash type: " + type);
         };
     }
 
     public static String toString(Multihash multihash, Multibase.Base base) {
         return Multibase.encode(base, multihash.toBytes());
+    }
+
+    public static String example(Multihash.Type type, Multibase.Base base) {
+        var bytes = new byte[type.length];
+        // for (int i = 0; i < type.length; i++) bytes[i] = (byte) 7;
+        var multihash = new Multihash(type, bytes);
+        return toString(multihash, base);
     }
 
     private Multihashes() {}
