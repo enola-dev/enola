ccf681a771bca969ead170e8fd32a03d178c294d
Michael Vorburger <mike@vorburger.ch>
2025-01-14 23:02:39 +0100

    feat  (web): Introduce string2long.ts class StringToLongBiMap

---
+++ web/src/string2long.ts
@@ -0,0 +1,50 @@
+/**
+ * Bidirectional map for common "Symbol" String to Long.
+ *
+ * Inspired by Java dev.enola.common.string2long.StringToLongBiMap.
+ */
+class StringToLongBiMap {
+  // TODO Write a test for this...
+
+  private symbolsMap: Map<string, number> = new Map()
+  private symbolsList: string[] = []
+  private nextId: number = 0
+
+  put(symbol: string): number {
+    if (this.nextId === Number.MAX_SAFE_INTEGER) {
+      throw new Error("Maximum number of symbols reached.")
+    }
+    const id = this.symbolsMap.get(symbol)
+    if (id !== undefined) {
+      return id
+    } else {
+      this.symbolsMap.set(symbol, this.nextId)
+      this.symbolsList[this.nextId] = symbol
+      return this.nextId++
+    }
+  }
+
+  getBySymbol(symbol: string): number {
+    const id = this.symbolsMap.get(symbol)
+    if (id === undefined) {
+      throw new Error(`Symbol not found: ${symbol}`)
+    }
+    return id
+  }
+
+  getById(id: number): string {
+    if (id >= 0 && id < this.symbolsList.length) {
+      return this.symbolsList[id]
+    } else {
+      throw new Error(`ID not found: ${id}`)
+    }
+  }
+
+  size(): number {
+    return this.nextId
+  }
+
+  symbols(): string[] {
+    return this.symbolsList
+  }
+}
