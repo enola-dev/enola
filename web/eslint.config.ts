21ec04e871413cc5412d7e232da797ee3b3fa0d6
Michael Vorburger <mike@vorburger.ch>
2025-01-15 00:40:37 +0100

    fix   (web): Adopt strict & stylistic tseslint

--- web/eslint.config.ts
+++ web/eslint.config.ts
@@ -1,11 +1,13 @@
 import eslint from "@eslint/js"
 import tseslint from "typescript-eslint"
 
-// https://typescript-eslint.io/getting-started/
 export default tseslint.config(
   {
     ignores: ["public/bundle.js", "web-out/**/*.js"],
   },
   eslint.configs.recommended,
-  tseslint.configs.recommended
+
+  // https://typescript-eslint.io/getting-started/
+  tseslint.configs.strict,
+  tseslint.configs.stylistic
 ) // TODO https://eslint.org/docs/latest/use/configure/configuration-files#typescript-configuration-files
