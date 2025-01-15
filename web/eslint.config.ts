5e1034f24b88677a1bbd27ce72b88b329c807d55
Michael Vorburger <mike@vorburger.ch>
2025-01-15 02:03:24 +0100

    fix   (web): Adopt strict & stylistic TypeChecked tseslint

--- web/eslint.config.ts
+++ web/eslint.config.ts
@@ -5,9 +5,20 @@ export default tseslint.config(
   {
     ignores: ["public/bundle.js", "web-out/**/*.js"],
   },
-  eslint.configs.recommended,
 
   // https://typescript-eslint.io/getting-started/
-  tseslint.configs.strict,
-  tseslint.configs.stylistic
+  // https://typescript-eslint.io/users/configs
+  // https://typescript-eslint.io/getting-started/typed-linting/
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  }
 ) // TODO https://eslint.org/docs/latest/use/configure/configuration-files#typescript-configuration-files
