d32ca46942380b525de9c3859a2df2d1eb5ac99a
Michael Vorburger <mike@vorburger.ch>
2025-01-17 22:36:53 +0100

    fix   (web): HTML not TS as entrypoint

--- web/src/bun/build.ts
+++ web/src/bun/build.ts
@@ -2,25 +2,30 @@
 
 import { build } from "bun"
 
+// TODO Adopt https://bun.sh/docs/bundler/fullstack
+
 // TODO Run both tsc & test from here, e.g. by using `concurrently` or `npm-run-all` (README.md)
 
-// TODO Support watch mode, ideally like https://esbuild.github.io/api/#live-reload
-
 const result = await build({
-  entrypoints: ["src/browser/index.ts"],
-  outdir: "public/bundles/",
+  html: true,
+  experimentalCss: true,
+
+  entrypoints: ["public/index.html"],
+  outdir: "web-out/",
 
   minify: true,
   sourcemap: "linked",
 
-  // TODO naming: "./[dir]/[name]-[hash].[ext]" BUT then how to reference it in the HTML?!
+  naming: "./[dir]/[name]-[hash].[ext]",
 
   throw: true,
 })
 
-if (!result.success) {
+if (result.success) {
+  // result.outputs.map(output => console.log("✅", output.path))
+  console.log("✅ Successfully 📦 bundled!")
+  process.exit(0)
+} else {
   console.error("Build failed:", result.logs)
   process.exit(1)
 }
-
-// TODO How to bundle CSS as well?
