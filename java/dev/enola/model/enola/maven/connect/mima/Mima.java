7b103ef71cf928c9c1a48ee4a234994cac13ad40
Michael Vorburger <mike@vorburger.ch>
2025-02-08 16:43:27 +0100

    fix (model): Factor out MIMA Maven Connector code into new class Mima

--- java/dev/enola/model/enola/maven/connect/mima/Mima.java
+++ java/dev/enola/model/enola/maven/connect/mima/Mima.java
@@ -17,4 +17,71 @@
  */
 package dev.enola.model.enola.maven.connect.mima;
 
-public class Mima {}
+import static eu.maveniverse.maven.mima.context.ContextOverrides.ChecksumPolicy.FAIL;
+import static eu.maveniverse.maven.mima.context.ContextOverrides.SnapshotUpdatePolicy.NEVER;
+
+import eu.maveniverse.maven.mima.context.Context;
+import eu.maveniverse.maven.mima.context.ContextOverrides;
+import eu.maveniverse.maven.mima.context.Runtimes;
+import eu.maveniverse.maven.mima.extensions.mmr.MavenModelReader;
+import eu.maveniverse.maven.mima.extensions.mmr.ModelRequest;
+import eu.maveniverse.maven.mima.extensions.mmr.ModelResponse;
+
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.resolution.ArtifactDescriptorException;
+import org.eclipse.aether.resolution.ArtifactResolutionException;
+import org.eclipse.aether.resolution.VersionResolutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Mima implements AutoCloseable {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final Context context;
+    private final MavenModelReader mmr;
+
+    public Mima() {
+        var overrides =
+                ContextOverrides.create()
+                        // TODO repositories() - ContextOverrides.AddRepositoriesOp?
+                        // TODO transferListener() to log progress?
+                        .checksumPolicy(FAIL)
+                        .snapshotUpdatePolicy(NEVER)
+                        .build();
+        // TODO https://maven.apache.org/resolver/configuration.html configProperties() ?!
+
+        var runtime = Runtimes.INSTANCE.getRuntime();
+        logger.info("Mima Maven Version: {}", runtime.mavenVersion());
+
+        this.context = runtime.create(overrides);
+        this.mmr = new MavenModelReader(context);
+    }
+
+    @Override
+    public void close() {
+        context.close();
+    }
+
+    /**
+     * Fetch a Maven Model from (remote) repositories, given a GAV.
+     *
+     * @param gav a "Gradle-style" GAV in the
+     *     <groupId>:<artifactId>[:<extension>[:<classifier>]]:<version> format, like e.g.
+     *     "ch.vorburger.mariaDB4j:mariaDB4j-core:3.1.0"
+     * @return a {@link ModelResponse}, of which you typically care about the {@link
+     *     ModelResponse#getEffectiveModel()}
+     */
+    public ModelResponse get(String gav)
+            throws ArtifactResolutionException,
+                    VersionResolutionException,
+                    ArtifactDescriptorException {
+        var request =
+                ModelRequest.builder()
+                        .setArtifact(new DefaultArtifact(gav))
+                        // TODO What is RequestContext really used for?!
+                        .setRequestContext(gav)
+                        .build();
+        var response = mmr.readModel(request);
+        return response;
+    }
+}
