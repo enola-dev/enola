c03a19b7b5071d2fea0cac29be6547038bf7e4a3
Michael Vorburger <mike@vorburger.ch>
2025-02-08 16:43:27 +0100

    fix (model): Factor out MIMA Maven Connector code into new class Mima

--- java/dev/enola/model/enola/maven/connect/mima/LearnMimaTest.java
+++ java/dev/enola/model/enola/maven/connect/mima/LearnMimaTest.java
@@ -19,54 +19,47 @@ package dev.enola.model.enola.maven.connect.mima;
 
 import static com.google.common.truth.Truth.assertThat;
 
-import static eu.maveniverse.maven.mima.context.ContextOverrides.ChecksumPolicy.FAIL;
-import static eu.maveniverse.maven.mima.context.ContextOverrides.SnapshotUpdatePolicy.NEVER;
-
-import eu.maveniverse.maven.mima.context.Context;
-import eu.maveniverse.maven.mima.context.ContextOverrides;
-import eu.maveniverse.maven.mima.context.Runtimes;
-import eu.maveniverse.maven.mima.extensions.mmr.MavenModelReader;
-import eu.maveniverse.maven.mima.extensions.mmr.ModelRequest;
-
 import org.eclipse.aether.RepositoryException;
-import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.resolution.ArtifactResolutionException;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class LearnMimaTest {
 
-    // TODO Move some stuff that's initially here into class Mima afterwards...
+    // TODO Origin repo
 
-    private final Logger logger = LoggerFactory.getLogger(getClass());
+    // TODO XML
+
+    // TODO Classpath
+
+    // TODO Test other repo URLs than Maven Central (and make explicit)
+
+    // TODO Allow explicit repo in get()
+
+    // TODO class/record GAV, instead String
+
+    // TODO Dependencies & Parent
+
+    // TODO interface Artifact extends Thing
 
     @Test
-    public void mima() throws RepositoryException {
-        var gav = "ch.vorburger.mariaDB4j:mariaDB4j-core:3.1.0";
-
-        var overrides =
-                ContextOverrides.create()
-                        // TODO repositories() - ContextOverrides.AddRepositoriesOp?
-                        // TODO transferListener() to log progress?
-                        .checksumPolicy(FAIL)
-                        .snapshotUpdatePolicy(NEVER)
-                        .build();
-        // TODO https://maven.apache.org/resolver/configuration.html configProperties() ?!
-
-        var runtime = Runtimes.INSTANCE.getRuntime();
-        logger.info("Mima Maven Version: {}", runtime.mavenVersion());
-
-        try (Context context = runtime.create(overrides)) {
-            var mmr = new MavenModelReader(context);
-            var request =
-                    ModelRequest.builder()
-                            .setArtifact(new DefaultArtifact(gav))
-                            // TODO What is RequestContext used for?!
-                            .setRequestContext("LearnMimaTest-demo")
-                            .build();
-            var response = mmr.readModel(request);
-            var model = response.getEffectiveModel();
+    public void mariaDB4j() throws RepositoryException {
+        try (var mima = new Mima()) {
+            var model = mima.get("ch.vorburger.mariaDB4j:mariaDB4j-core:3.1.0").getEffectiveModel();
             assertThat(model).isNotNull();
+        }
+    }
+
+    @Test(expected = ArtifactResolutionException.class)
+    public void nonExistingVersion() throws RepositoryException {
+        try (var mima = new Mima()) {
+            mima.get("ch.vorburger.mariaDB4j:mariaDB4j-core:1.0.0");
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void gavWithoutVersion() throws RepositoryException {
+        try (var mima = new Mima()) {
+            mima.get("ch.vorburger.mariaDB4j:mariaDB4j-core");
         }
     }
 }
