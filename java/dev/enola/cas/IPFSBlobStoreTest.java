86fe3e0b20170d31fa2b9de9dce03625cc939f00
Michael Vorburger <mike@vorburger.ch>
2025-02-28 17:43:49 +0100

    fix  (core): Implement IPFSBlobStore.store()

--- java/dev/enola/cas/IPFSBlobStoreTest.java
+++ java/dev/enola/cas/IPFSBlobStoreTest.java
@@ -19,28 +19,44 @@ package dev.enola.cas;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import com.google.common.io.ByteSource;
+
 import dev.enola.common.context.testlib.SingletonRule;
 import dev.enola.common.io.mediatype.MediaTypeProviders;
-import dev.enola.common.io.resource.OkHttpResource;
 
+import io.ipfs.api.IPFS;
 import io.ipfs.cid.Cid;
 
 import org.junit.Rule;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Random;
 
 public class IPFSBlobStoreTest {
 
     public @Rule SingletonRule singleton = SingletonRule.$(MediaTypeProviders.set());
 
+    IPFSBlobStore ipfs = new IPFSBlobStore(new IPFS("/ip4/127.0.0.1/tcp/5001"));
+
     @Test
     public void hello() throws IOException {
-        var ipfs =
-                new IPFSBlobStore(
-                        new IPFSResource.Provider(
-                                new OkHttpResource.Provider(), "http://localhost:8080/ipfs/"));
         var bytes = ipfs.load(Cid.decode("QmXV7pL1CB7A8Tzk7jP2XE9kRyk8HZd145KDptdxzmNLfu"));
         assertThat(new String(bytes.read())).isEqualTo("hello, world\n");
+    }
+
+    @Test
+    public void random() throws IOException {
+        var bytes = generateRandomBytes(1024);
+        var cid = ipfs.store(ByteSource.wrap(bytes));
+        var loaded = ipfs.load(cid);
+        assertThat(loaded.read()).isEqualTo(bytes);
+    }
+
+    private static byte[] generateRandomBytes(int length) {
+        byte[] bytes = new byte[length];
+        Random random = new Random();
+        random.nextBytes(bytes);
+        return bytes;
     }
 }
