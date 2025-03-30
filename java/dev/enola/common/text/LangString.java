b6b9618f8b35bb6f832f7641965ff047571a11e4
Michael Vorburger <mike@vorburger.ch>
2025-03-30 15:01:51 +0200

    api: Introduce new class LangString

---
+++ java/dev/enola/common/text/LangString.java
@@ -0,0 +1,122 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2025 The Enola <https://enola.dev> Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.enola.common.text;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.Locale;
+
+/**
+ * LangString is a String plus its (mandatory) language.
+ *
+ * <p>For example, <i>"hello, world" in English</i> is a LangString.
+ *
+ * <p>Text Direction is optionally also part of a LangString.
+ *
+ * <p>The class name <tt>LangString</tt> was chosen both because of its <i>*String</i> suffix
+ * familiarity for Java developers, and inspired by <tt>rdf:langString</tt> (even though this Java
+ * type is otherwise technically not specific to the Resource Description Framework [RDF] as such).
+ *
+ * <p>Alternative names for the same concept include <tt>LocalizedText</tt>, <tt>LanguageText</tt>,
+ * <tt>MultilingualString</tt> ('MLS'), <tt>InternationalizedString</tt> or <tt>LanguageString</tt>.
+ */
+public sealed class LangString {
+
+    private final String text;
+    private final Locale language;
+
+    private LangString(String text, Locale language) {
+        this.text = requireNonNull(text, "text");
+        this.language = requireNonNull(language, "language");
+    }
+
+    public static LangString of(String text, Locale language) {
+        return new LangString(text, language);
+    }
+
+    public static LangString of(String text, Locale language, Direction direction) {
+        return new DirectionalLangString(text, language, direction);
+    }
+
+    public String text() {
+        return text;
+    }
+
+    public Locale language() {
+        return language;
+    }
+
+    public Direction direction() {
+        return Direction.AUTO;
+    }
+
+    @Override
+    public String toString() {
+        return "\"" + text + "\"@" + language.toLanguageTag();
+    }
+
+    @Override
+    public int hashCode() {
+        int result = text.hashCode();
+        result = 31 * result + language.hashCode();
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        LangString that = (LangString) o;
+        return text.equals(that.text) && language.equals(that.language);
+    }
+
+    private static final class DirectionalLangString extends LangString {
+        private final Direction direction;
+
+        private DirectionalLangString(String text, Locale language, Direction direction) {
+            super(text, language);
+            this.direction = requireNonNull(direction, "direction");
+        }
+
+        @Override
+        public Direction direction() {
+            return direction;
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + "-" + direction;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = super.hashCode();
+            result = 31 * result + direction.hashCode();
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || getClass() != o.getClass()) return false;
+            if (!super.equals(o)) return false;
+
+            DirectionalLangString that = (DirectionalLangString) o;
+            return direction == that.direction;
+        }
+    }
+}
