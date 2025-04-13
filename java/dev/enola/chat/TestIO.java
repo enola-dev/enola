a7b5b3a4baabf161a6ced42b36099737bef4c883
Michael Vorburger <mike@vorburger.ch>
2025-04-13 18:18:37 +0200

    feat: Initial super basic Chat Demo

---
+++ java/dev/enola/chat/TestIO.java
@@ -0,0 +1,46 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2025 The Enola <https://enola.dev> Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.enola.chat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestIO implements IO {
+
+    private int line = 0;
+    private final List<String> input;
+    private final List<String> output = new ArrayList<>();
+
+    public TestIO(List<String> lines) {
+        this.input = lines;
+    }
+
+    @Override
+    public String readLine() {
+        return input.get(line++);
+    }
+
+    @Override
+    public void printf(String format, Object... args) {
+        output.add(String.format(format, args));
+    }
+
+    public List<String> getOutput() {
+        return output;
+    }
+}
