9b8b9607706f4df4f482457974df44e4744deca9
Michael Vorburger <mike@vorburger.ch>
2025-05-29 18:15:01 +0200

    clean: mv 'old' exec infra to common.exec.vorburger

--- java/dev/enola/common/exec/vorburger/VorburgerExecRunner.java
+++ java/dev/enola/common/exec/vorburger/VorburgerExecRunner.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package dev.enola.common.exec;
+package dev.enola.common.exec.vorburger;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
