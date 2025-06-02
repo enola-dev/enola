5171031565d2f3cc20ce2e34d077ef5ed5705213
Michael Vorburger <mike@vorburger.ch>
2025-06-02 10:16:56 +0200

    wip: Try to use record-builder with Maven instead of Bazel

--- java/dev/enola/common/struct/Placeholder.java
+++
@@ -1,6 +0,0 @@
-package dev.enola.common.struct;
-
-import io.soabase.recordbuilder.core.RecordInterface;
-
-@RecordInterface.Include({Example.class})
-public class Placeholder {} // TODO Remove again, when problem is solved.
