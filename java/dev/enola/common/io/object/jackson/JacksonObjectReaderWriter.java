d1336dd9b0a932611ea469b63d7c9f50876b6b8a
Michael Vorburger <mike@vorburger.ch>
2025-06-07 23:03:37 +0200

    feat: common.io.object.jackson.YamlObjectReader extends JacksonObjectReader

---
+++ java/dev/enola/common/io/object/jackson/JacksonObjectReaderWriter.java
@@ -0,0 +1,63 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2025 The Enola <https://enola.dev> Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.enola.common.io.object.jackson;
+
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.net.MediaType;
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
+
+import dev.enola.common.io.object.ObjectReader;
+import dev.enola.common.io.object.ObjectWriter;
+import dev.enola.common.io.resource.ReadableResource;
+import dev.enola.common.io.resource.WritableResource;
+
+import java.io.IOException;
+import java.util.Optional;
+
+abstract class JacksonObjectReaderWriter implements ObjectReader, ObjectWriter {
+
+    private final ObjectMapper mapper;
+
+    protected JacksonObjectReaderWriter(ObjectMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    abstract boolean canHandle(MediaType mediaType);
+
+    @Override
+    public <T> Optional<T> optional(ReadableResource resource, Class<T> type) throws IOException {
+        if (!canHandle(resource.mediaType())) return Optional.empty();
+        try (var reader = resource.charSource().openBufferedStream()) {
+            return Optional.of(mapper.readValue(reader, type));
+        } catch (DatabindException e) {
+            throw new IOException(e);
+        }
+    }
+
+    @CanIgnoreReturnValue
+    public boolean write(Object instance, WritableResource resource) throws IOException {
+        if (!canHandle(resource.mediaType())) return false;
+        try (var writer = resource.charSink().openBufferedStream()) {
+            mapper.writeValue(writer, instance);
+            return true;
+        } catch (DatabindException e) {
+            throw new IOException(e);
+        }
+    }
+}
--- java/dev/enola/common/io/object/jackson/package-info.java
+++ java/dev/enola/common/io/object/jackson/package-info.java
@@ -1,7 +1,7 @@
 /*
  * SPDX-License-Identifier: Apache-2.0
  *
- * Copyright 2024-2025 The Enola <https://enola.dev> Authors
+ * Copyright 2025 The Enola <https://enola.dev> Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,20 +15,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package dev.enola.common.yamljson;
 
-import static com.google.common.truth.Truth.assertThat;
+/** Object I/O with Samuel L. Jackson. */
+@NullMarked
+package dev.enola.common.io.object.jackson;
 
-import org.junit.Test;
-
-import java.time.Instant;
-
-public class JSONTest {
-
-    // See YamlJsonTest for more tests.
-
-    @Test
-    public void instant() {
-        assertThat(JSON.write(Instant.now(), false)).isNotEmpty();
-    }
-}
+import org.jspecify.annotations.NullMarked;
