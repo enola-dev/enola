bcfba4b645986eb1b160cb55b6d4dd4a4fe629ac
Michael Vorburger <mike@vorburger.ch>
2025-06-09 12:47:48 +0200

    clean: Move GoogleBaseLlmProvider etc. from ai.agent to ai.adk.iri

--- java/dev/enola/ai/adk/iri/GoogleBaseLlmProvider.java
+++ java/dev/enola/ai/adk/iri/GoogleBaseLlmProvider.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package dev.enola.ai.agent;
+package dev.enola.ai.adk.iri;
 
 import com.google.adk.models.BaseLlm;
 import com.google.adk.models.Gemini;
