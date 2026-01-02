7d04bd656d15ca199febb1382c1df81f3d257520
Michael Vorburger <mike@vorburger.ch>
2026-01-02 01:55:27 +0100

    clean: Move YAML ObjectMapper from YamlObjectReaderWriter to ObjectMappers

--- java/dev/enola/common/io/object/jackson/YamlObjectReaderWriter.java
+++ java/dev/enola/common/io/object/jackson/YamlObjectReaderWriter.java
@@ -1,7 +1,7 @@
 /*
  * SPDX-License-Identifier: Apache-2.0
  *
- * Copyright 2025 The Enola <https://enola.dev> Authors
+ * Copyright 2025-2026 The Enola <https://enola.dev> Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,52 +17,21 @@
  */
 package dev.enola.common.io.object.jackson;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
-import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import com.google.common.net.MediaType;
 
 import dev.enola.common.io.mediatype.MediaTypes;
 import dev.enola.common.io.mediatype.YamlMediaType;
 import dev.enola.common.io.object.ProviderFromID;
-
-import org.yaml.snakeyaml.DumperOptions;
-import org.yaml.snakeyaml.LoaderOptions;
+import dev.enola.common.jackson.ObjectMappers;
 
 public class YamlObjectReaderWriter extends JacksonObjectReaderWriter {
 
     public YamlObjectReaderWriter() {
-        super(newObjectMapper());
+        super(ObjectMappers.YAML);
     }
 
     public YamlObjectReaderWriter(ProviderFromID provider) {
-        super(newObjectMapper(), provider);
-    }
-
-    private static ObjectMapper newObjectMapper() {
-        // NB: Keep in-sync with the similar (but not the same, different API!) in
-        //       the dev.enola.common.yamljson.YAML class.
-        var loaderOptions = new LoaderOptions();
-        loaderOptions.setAllowDuplicateKeys(false);
-        loaderOptions.setAllowRecursiveKeys(false);
-        loaderOptions.setCodePointLimit(10 * 1024 * 1024); // 10 MB
-
-        var dumperOptions = new DumperOptions();
-        dumperOptions.setExplicitStart(false);
-        dumperOptions.setDefaultScalarStyle(DumperOptions.ScalarStyle.PLAIN);
-
-        var yamlFactory =
-                YAMLFactory.builder()
-                        .loaderOptions(loaderOptions)
-                        .dumperOptions(dumperOptions)
-                        .build();
-
-        var yamlMapper = new YAMLMapper(yamlFactory);
-        yamlMapper.disable(YAMLGenerator.Feature.WRITE_DOC_START_MARKER);
-        yamlMapper.enable(YAMLGenerator.Feature.MINIMIZE_QUOTES);
-
-        return yamlMapper;
+        super(ObjectMappers.YAML, provider);
     }
 
     @Override
