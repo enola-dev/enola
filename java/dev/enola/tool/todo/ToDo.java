aae11b9c3ef787b7863bb3b1b7c8f63d03e138f0
Michael Vorburger <vorburger@google.com>
2026-03-25 18:30:39 +0100

    clean: class ToDo as record with Builder

--- java/dev/enola/tool/todo/ToDo.java
+++ java/dev/enola/tool/todo/ToDo.java
@@ -17,51 +17,119 @@
  */
 package dev.enola.tool.todo;
 
+import com.google.auto.value.AutoBuilder;
+
 import dev.enola.data.id.UUID_IRI;
+
+import org.jspecify.annotations.Nullable;
 
 import java.net.URI;
 import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 
-public class ToDo {
-    // TODO implements Identifiable<URI>
+public record ToDo(
+        // TODO implements Identifiable<URI>
+        URI id,
+        String title,
+        @Nullable String description,
+        List<String> tags,
+        Map<String, String> attributes,
+        @Nullable Instant created,
+        @Nullable Instant modified,
+        @Nullable Instant completed,
+        @Nullable Boolean isCompleted,
+        @Nullable Byte priority,
 
-    // TODO Generate record and Builder, from a model; but for now, this is good enough
+        /** Assignee URI; e.g. <code>mailto:user@example.org</code>. */
+        @Nullable URI assignee,
 
-    public URI id;
-    public String title;
-    public String description;
+        /** Parent ToDo ID; e.g. a "Project". */
+        // TODO Add other more flexible relationships, e.g. "depends on", "related to" etc.
+        // TODO public Set<URI> children = new HashSet<>();
+        @Nullable URI parent
 
-    public final List<String> tags = new java.util.ArrayList<>();
-    public final Map<String, String> attributes = new java.util.HashMap<>();
+        // TODO Discussion, like in a bug tracker, with comments by users etc.
 
-    public Instant created;
-    public Instant modified;
-    public Instant completed;
-    public Boolean isCompleted;
+        // TODO public Set<URI> attachments = new HashSet<>();
+        ) {
 
-    // status is intentionally omitted, because different backends will have differences.
-    public Byte priority;
+    public static Builder builder() {
+        return new AutoBuilder_ToDo_Builder();
+    }
 
-    /** Assignee URI; e.g. <code>mailto:user@example.org</code>. */
-    public URI assignee;
+    public Builder toBuilder() {
+        return builder().from(this);
+    }
 
-    /** Parent ToDo ID; e.g. a "Project". */
-    // TODO Add other more flexible relationships, e.g. "depends on", "related to" etc.
-    // TODO public Set<URI> children = new HashSet<>();
-    public URI parent;
+    @AutoBuilder(callMethod = "create")
+    public abstract static class Builder {
+        public abstract Builder id(@Nullable URI id);
 
-    // TODO Discussion, like in a bug tracker, with comments by users etc.
+        public abstract Builder title(String title);
 
-    // TODO public Set<URI> attachments = new HashSet<>();
+        public abstract Builder description(@Nullable String description);
 
-    public void prepareForSave() {
-        if (title == null) throw new IllegalStateException("Task title is required: " + this);
+        public abstract Builder tags(@Nullable List<String> tags);
+
+        public abstract Builder attributes(@Nullable Map<String, String> attributes);
+
+        public abstract Builder created(@Nullable Instant created);
+
+        public abstract Builder modified(@Nullable Instant modified);
+
+        public abstract Builder completed(@Nullable Instant completed);
+
+        public abstract Builder isCompleted(@Nullable Boolean isCompleted);
+
+        public abstract Builder priority(@Nullable Byte priority);
+
+        public abstract Builder assignee(@Nullable URI assignee);
+
+        public abstract Builder parent(@Nullable URI parent);
+
+        public Builder from(ToDo todo) {
+            return id(todo.id())
+                    .title(todo.title())
+                    .description(todo.description())
+                    .tags(todo.tags())
+                    .attributes(todo.attributes())
+                    .created(todo.created())
+                    .modified(todo.modified())
+                    .completed(todo.completed())
+                    .isCompleted(todo.isCompleted())
+                    .priority(todo.priority())
+                    .assignee(todo.assignee())
+                    .parent(todo.parent());
+        }
+
+        public abstract ToDo build();
+    }
+
+    static ToDo create(
+            @Nullable URI id,
+            String title,
+            @Nullable String description,
+            @Nullable List<String> tags,
+            @Nullable Map<String, String> attributes,
+            @Nullable Instant created,
+            @Nullable Instant modified,
+            @Nullable Instant completed,
+            @Nullable Boolean isCompleted,
+            @Nullable Byte priority,
+            @Nullable URI assignee,
+            @Nullable URI parent) {
+
+        if (title == null) throw new IllegalStateException("Task title is required");
         if (id == null) id = new UUID_IRI().toURI();
+        if (tags == null) tags = List.of();
+        if (attributes == null) attributes = Map.of();
 
-        if (created == null) created = Instant.now();
-        else modified = Instant.now();
+        if (created == null) {
+            created = Instant.now();
+        } else {
+            modified = Instant.now();
+        }
         if (isCompleted != null) {
             if (isCompleted) {
                 completed = Instant.now();
@@ -69,5 +137,18 @@ public class ToDo {
                 completed = null;
             }
         }
+        return new ToDo(
+                id,
+                title,
+                description,
+                tags,
+                attributes,
+                created,
+                modified,
+                completed,
+                isCompleted,
+                priority,
+                assignee,
+                parent);
     }
 }
